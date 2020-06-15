0392ac8a2c89be2abc3797c0d29c4bda43c211ad
Phil Chen <06fahchen@gmail.com>
2020-06-15 22:01:20 +0800

    refactor to_po_file and to_po_binary

--- lib/i18next_to_gettext.ex
+++ lib/i18next_to_gettext.ex
@@ -5,24 +5,32 @@ defmodule I18nextToGettext do
 
   alias I18nextToGettext.Phase
 
-  def to_po_file(input_file, output_file) do
-    input_file
-    |> Phase.ReadJsonFile.run()
-    |> Phase.DecodeJsonBinary.run()
-    |> Phase.FlatJson.run()
-    |> Phase.ExtractValues.ReplaceInterpolation.run()
-    |> Phase.ExtractValues.ReplaceNest.run()
-    |> Phase.GeneratePoBinary.run()
+  def to_po_file({:binary, binary}, output_file) do
+    binary
+    |> to_po_binary()
     |> Phase.WritePoBinaryToFile.run(output_file)
   end
 
-  def to_po_binary(input_file) do
-    input_file
-    |> Phase.ReadJsonFile.run()
+  def to_po_file(input, output_file) do
+    input
+    |> to_po_binary()
+    |> Phase.WritePoBinaryToFile.run(output_file)
+  end
+
+  def to_po_binary({:binary, binary}) do
+    binary
     |> Phase.DecodeJsonBinary.run()
     |> Phase.FlatJson.run()
     |> Phase.ExtractValues.ReplaceInterpolation.run()
     |> Phase.ExtractValues.ReplaceNest.run()
     |> Phase.GeneratePoBinary.run()
+  end
+
+  def to_po_binary({:path, input_file}) do
+    to_po_binary({:binary, Phase.ReadJsonFile.run(input_file)})
+  end
+
+  def to_po_binary(input_file) do
+    to_po_binary({:path, input_file})
   end
 end
