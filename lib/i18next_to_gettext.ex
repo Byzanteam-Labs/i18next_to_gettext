49b555d878da896458767f8874b499d8f92bbed8
Phil Chen <06fahchen@gmail.com>
2020-06-15 22:12:45 +0800

    feat: to_po_file/binary from a map

--- lib/i18next_to_gettext.ex
+++ lib/i18next_to_gettext.ex
@@ -5,25 +5,22 @@ defmodule I18nextToGettext do
 
   alias I18nextToGettext.Phase
 
-  def to_po_file({:binary, binary}, output_file) do
-    binary
-    |> to_po_binary()
-    |> Phase.WritePoBinaryToFile.run(output_file)
-  end
-
   def to_po_file(input, output_file) do
     input
     |> to_po_binary()
     |> Phase.WritePoBinaryToFile.run(output_file)
   end
 
-  def to_po_binary({:binary, binary}) do
+  def to_po_binary({:map, binary}) do
     binary
-    |> Phase.DecodeJsonBinary.run()
     |> Phase.FlatJson.run()
     |> Phase.ExtractValues.ReplaceInterpolation.run()
     |> Phase.ExtractValues.ReplaceNest.run()
     |> Phase.GeneratePoBinary.run()
+  end
+
+  def to_po_binary({:binary, binary}) do
+    to_po_binary({:map, Phase.DecodeJsonBinary.run(binary)})
   end
 
   def to_po_binary({:path, input_file}) do
